501bc764cd246c144a6a7335e666bd508f4cd986
macournoyer <macournoyer@gmail.com>
2011-09-20 14:56:37 -0400

    Implement intercepting filter pattern.

---
+++ tests/filters_test.rb
@@ -0,0 +1,34 @@
+require File.dirname(__FILE__) + '/test_helper'
+require "controller"
+require "filters"
+
+class TestController < Controller
+  before_filter :one
+  before_filter :two
+  
+  def initialize(out)
+    @out = out
+  end
+  
+  def one
+    @out << :one
+  end
+  
+  def two
+    @out << :two
+  end
+end
+
+class FiltersTest < Test::Unit::TestCase
+  def test_store_filters
+    assert_equal [:one, :two], TestController.before_filters
+  end
+  
+  def test_filter
+    out = []
+    TestController.new(out).filter do
+      out << :process
+    end
+    assert_equal [:one, :two, :process], out
+  end
+end
