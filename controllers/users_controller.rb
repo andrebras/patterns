2a5f2175433aecbdd13877437de390dc9f654a9f
macournoyer <macournoyer@gmail.com>
2011-09-20 14:40:27 -0400

    Implement UsersController.

---
+++ controllers/users_controller.rb
@@ -0,0 +1,15 @@
+require "active_record"
+require "models/user"
+
+class UsersController < Controller
+  def index
+    User.all.each do |user|
+      response.write "<p>#{user.name}</p>"
+    end
+  end
+  
+  def show
+    @user = User.find(request["id"])
+    response.write "<p>#{@user.name}</p>"
+  end
+end
